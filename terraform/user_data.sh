22b51100fcdfed2557dc855a707cc8a7694c6088
Abhishekaa5 <115134934+Abhishekaa5@users.noreply.github.com>
2026-02-21 19:52:22 +0530

    Add AWS CLI installation to user data script

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -3,6 +3,6 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-user
-
+sudo yum install -y aws-cli
 # Install CloudWatch Agent
 sudo yum install -y amazon-cloudwatch-agent
