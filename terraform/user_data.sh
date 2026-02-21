c7069215b2f07a9ac42a2a5a0882f65b06cb7850
Abhishekaa5 <115134934+Abhishekaa5@users.noreply.github.com>
2026-02-21 17:44:40 +0530

    Add sudo to package installation commands

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-yum update -y
-yum install -y docker
-systemctl start docker
-usermod -aG docker ec2-user
+sudo yum update -y
+sudo yum install -y docker
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
 
 # Install CloudWatch Agent
-yum install -y amazon-cloudwatch-agent
+sudo yum install -y amazon-cloudwatch-agent
