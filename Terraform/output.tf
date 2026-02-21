6a38702b8a943f4b3b9fd8e9e68275ceebebfba3
Abhishek <patelabhishekaa5@gmail.com>
2026-02-21 17:09:00 +0530

    Force remove Terraform folder

--- Terraform/output.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip" {
-  value = aws_instance.boomibulls_web.public_ip
-}
-
-output "ecr_url" {
-  value = aws_ecr_repository.boomibulls_repo.repository_url
-}
--- Terraform/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "region" {}
-variable "key_name" {}
