f09fc61784c9076b4c76b23e7f979416a32e023c
Abhishek <patelabhishekaa5@gmail.com>
2026-02-22 11:35:51 +0530

    update the code

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ resource "aws_vpc" "boomibulls_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
+# Create a public subnet
 resource "aws_subnet" "boomibulls_public" {
   vpc_id                  = aws_vpc.boomibulls_vpc.id
   cidr_block              = "10.0.1.0/24"
